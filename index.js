afbb328226fbac4aec18b40d1b1687035bfb5d30
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-14 11:41:17 -0400

    working 3 units

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const convertButtonEl = document.getElementById("convert-btn");
 const inputEl = document.getElementById("input-value");
 const lengthEl = document.getElementById("length-p");
+const volumeEl = document.getElementById("volume-p");
+const massEl = document.getElementById("mass-p");
 
 convertButtonEl.addEventListener("click", function(){
     console.log(inputEl.value);
     convert();
-    lengthEl.innerHTML = `<p> ${inputEl.value} meters = 0.000 feet | 0 feet = 0.000 meters </p>`
     inputEl.value = "";
 })
 
 function convert(){
-    let feetMeter = inputEl.value * 3.281;
-    console.log(feetMeter)
+    const feetToMeter = inputEl.value / 3.281;
+    const meterToFeet  = inputEl.value * 3.281;
+    const litersToGallons = inputEl.value * 0.264;
+    const gallosnToLiters = inputEl.value / 0.264;
+    const kilogramsToPounds = inputEl.value * 2.204;
+    const poundsToKilograms = inputEl.value / 2.204;
+    lengthEl.innerHTML = `<p> ${inputEl.value} meters = ${meterToFeet.toFixed(2)} feet | ${inputEl.value} feet = ${feetToMeter.toFixed(2)} meters </p>`
+    volumeEl.innerHTML = `<p> ${inputEl.value} liters = ${litersToGallons.toFixed(2)} gallons | ${inputEl.value} gallons = ${gallosnToLiters.toFixed(2)} liters </p>`
+    massEl.innerHTML = `<p> ${inputEl.value} kilograms = ${kilogramsToPounds.toFixed(2)} pounds | ${inputEl.value} pounds = ${poundsToKilograms.toFixed(2)} kilograms </p>`
 }
